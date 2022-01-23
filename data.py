97629975a71acfaf7d61f689696ce0df5b9dea89
Pliszka <karoliniakkjk@gmail.com>
2022-01-23 23:55:29 +0100

    New database

--- data.py
+++ data.py
@@ -15,3 +15,27 @@ question_data = [
              " in half more than 7 times.", "answer": "False"},
     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
 ]
+
+newQuestions = [
+    {"category": "Entertainment: Books", "type": "boolean", "difficulty": "easy",
+      "question": "The 'Berenstein Bears' is the correct spelling of the educational children's book series name.",
+      "correct_answer": "False", "incorrect_answers": ["True"]},
+     {"category": "Entertainment: Books", "type": "boolean", "difficulty": "easy",
+      "question": "The book 1984 was published in 1949.", "correct_answer": "True",
+      "incorrect_answers": ["False"]},
+     {"category": "Entertainment: Books", "type": "boolean", "difficulty": "easy",
+      "question": "Stephen Chbosky wrote the book &#039;The Perks of Being A Wallflower&#039;.",
+      "correct_answer": "True", "incorrect_answers": ["False"]},
+     {"category": "Entertainment: Books", "type": "boolean", "difficulty": "easy",
+      "question": "&quot;Elementary, my dear Watson&quot; is a phrase that is never truly said within the Conan Doyle books of Sherlock Holmes.",
+      "correct_answer": "True", "incorrect_answers": ["False"]},
+     {"category": "Entertainment: Books", "type": "boolean", "difficulty": "easy",
+      "question": "&quot;Green Eggs and Ham&quot; consists of only 50 different words.",
+      "correct_answer": "True", "incorrect_answers": ["False"]},
+     {"category": "Entertainment: Books", "type": "boolean", "difficulty": "easy",
+      "question": "The Harry Potter series of books, combined, are over 1,000,000 words in length.",
+      "correct_answer": "True", "incorrect_answers": ["False"]},
+     {"category": "Entertainment: Books", "type": "boolean", "difficulty": "easy",
+      "question": "Shub-Niggurath is a creature that was created by \tJ. R. R. Tolkien in his novel &quot;The Lord of The Rings&quot;.",
+      "correct_answer": "False", "incorrect_answers": ["True"]}
+]
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from question_model import Question
-from data import question_data
+from data import question_data, newQuestions
 from quiz_brain import QuizBrain
 
 questionBank = []
-for dict in question_data:
-    questions = Question(dict['text'], dict['answer'])
+# for dict in question_data:
+#     questions = Question(dict['text'], dict['answer'])
+#     questionBank.append(questions)
+
+for dict in newQuestions:
+    questions = Question(dict['question'], dict['correct_answer'])
     questionBank.append(questions)
 
 quiz=QuizBrain(questionBank)
